e9004207990d502a8fb440048c853f098e39811a
Andron00e <semenov.andrei.v@gmail.com>
2021-04-05 22:44:48 +0300

    Updates

---
+++ 8lab/Selection/Selection.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+using namespace std;
+
+const unsigned int size = 100000;
+unsigned int a_max = 100;
+
+void print_array(int * array, unsigned int size){
+    for (int i = 0; i < size; i++){
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+}
+
+void selection_sort(int * array, unsigned int size){
+    int temp, min;
+    for (int i = 0; i < size; i++){
+        min = i;
+        for (int j = i + 1; j < size; j++){
+            if (array[j] < array[min]){
+                min = j;
+            }
+        }
+
+        temp = array[min];
+        for (int k = min; k > i; k--) {
+            array[k] = array[k - 1];
+        }
+        array[i] = temp;
+    }
+}
+
+int random(int a){
+    int result = rand() % (a + 1);
+    if (rand() % 2 == 1){
+        result = - result;
+    }
+    return result;
+}
+
+void put_random(int * array, unsigned int size){
+    for (int i = 0; i < size; i++){
+        array[i] = random(a_max);
+    }
+}
+
+int main() {
+    srand(time(0));
+
+    int array [size];
+
+    put_random(array, size);
+    print_array(array, size);
+
+    auto start = chrono::steady_clock::now();
+    selection_sort(array, size);
+    auto finish = chrono::steady_clock::now();
+    print_array(array, size);
+
+    cout << (finish - start).count();
+    return 0;
+}
